1b2c90cf47670bdd247b57a3c72518ff4c4b71b9
Arnaud.michel <arnaud.michel@epitech.eu>
2019-10-29 18:33:29 +0100

    carousel

--- web/src/components/project-preview-grid.js
+++ web/src/components/project-preview-grid.js
@@ -1,21 +1,21 @@
 import { Link } from 'gatsby'
 import React from 'react'
 import ProjectPreview from './project-preview'
-
 import styles from './project-preview-grid.module.css'
+import { Carousel } from 'react-bootstrap'
 
 function ProjectPreviewGrid(props) {
   return (
     <div className={styles.root}>
       {props.title && <h2 className={styles.headline}>{props.title}</h2>}
-      <ul className={styles.grid}>
-        {props.nodes &&
-          props.nodes.map(node => (
-            <li key={node.id}>
-              <ProjectPreview {...node} />
-            </li>
-          ))}
-      </ul>
+      <Carousel>
+          {props.nodes &&
+            props.nodes.map(node => (
+                <Carousel.Item key={node.id}>
+                  <ProjectPreview {...node} />
+                </Carousel.Item>
+            ))}
+      </Carousel>
       {props.browseMoreHref && (
         <div className={styles.browseMoreNav}>
           <Link to={props.browseMoreHref}>Browse more</Link>
